24fe19f898cbe123b884aa332fa206133646b379
amaan <amaanx20@hotmail.co.uk>
2023-11-09 00:28:53 +0000

    commit

---
+++ week1.py
@@ -0,0 +1,13 @@
+
+print("Hello World")
+
+clesius=38.4
+fahrenheit_temp = (clesius*9/5)+32
+print("The Fahrenheit eqivalent of 38.4 celsius =", fahrenheit_temp)
+
+name = input("What is your name?")
+print("Hello" + name, "Nice to meet you!")
+
+print("Would you like to know the score of the cricket game?")
+input("")
+print(48426/852)
