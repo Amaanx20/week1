750265b1d613ece47b867a5b5bcbbc0fca0253fb
amaan <amaanx20@hotmail.co.uk>
2023-12-01 13:05:47 +0000

    commit

--- week1.py
+++ week1.py
@@ -11,3 +11,6 @@ print("Hello" + name, "Nice to meet you!")
 print("Would you like to know the score of the cricket game?")
 input("")
 print(48426/852)
+
+print("the number of lab groups for the university are.",300/24)
+print("the remainder is 0.5")
